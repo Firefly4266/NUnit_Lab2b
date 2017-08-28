1683f8c71be6162727d6ed4706c479195b13d00b
Firefly4266 <gaines.johnp@gmail.com>
2017-08-28 16:45:47 -0700

    Initial customers

---
+++ AcmeTest/CustomersFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace AcmeTest
+{
+    [TestFixture]
+    class CustomersFixture
+    {
+        private Customers custs;
+        private void ShowCustomerArray(Customer[] arr)
+        {
+            while (arr !=null)
+            {
+                int index = 0;
+                string id = arr[index].CustomerId.ToString();
+                string firstName = arr[index].FirstName;
+                string lastName = arr[index].LastName;
+                string emailAddress = arr[index].EmailAddress;
+                string str = id + firstName + lastName + emailAddress;
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
